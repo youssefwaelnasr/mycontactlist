314a536724d905914dc5f237610a853b19284b13
Youssef Wael <youssefwaelnasr@gmail.com>
2020-11-16 17:21:26 +0200

    Added drag and drop sorting for contacts and fixed some bugs

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
+const contactList = document.querySelector("#list");
 const searchDiv = document.querySelector("#search-div");
-let open = false;
 const searchBar = document.querySelector("#search");
 const addForm = document.forms["add-form"];
-const contactList = document.querySelector("#list");
 const noContactsMessage = document.querySelector("#no-contacts");
 
-// ADD EVENT LISTENERS
 searchDiv.addEventListener("click", showHideSearch);
 searchBar.addEventListener("keyup", searchContacts);
 addForm.addEventListener("submit", addContact);
 contactList.addEventListener("click", deleteContact);
+contactList.addEventListener("dragstart", dragStart);
+contactList.addEventListener("dragend", dragEnd);
+contactList.addEventListener("dragover", dragOver);
 
-// FUNCTION: SHOW/HIDE SEARCH BAR ON BUTTON CLICK
+let open = false;
 function showHideSearch(e) {
-    e.preventDefault;
     const searchBar = document.querySelector("#search");
     const searchButton = document.querySelector("#search-button");
 
@@ -36,7 +36,6 @@ function showHideSearch(e) {
     }
 }
 
-// FUNCTION: SEARCH CONTACTS
 function searchContacts(e) {
     const searchText = e.target.value.toLowerCase();
     const contactNames = contactList.getElementsByTagName("li");
@@ -50,7 +49,6 @@ function searchContacts(e) {
     });
 }
 
-// FUNCTION: ADD NEW CONTACT
 function addContact(e) {
     e.preventDefault();
     const nameValue = addForm.querySelector("#name-input").value;
@@ -71,25 +69,43 @@ function addContact(e) {
     delIcon.className = "fas fa-user-minus";
     deleteButton.appendChild(delIcon);
 
+    li.setAttribute("draggable", true);
     li.appendChild(nameSpan);
     li.appendChild(numberSpan);
     li.appendChild(deleteButton);
     contactList.appendChild(li);
+
+    addForm.reset();
 
     if (contactList.children.length >= 1) {
         noContactsMessage.style.display = "none";
     }
 }
 
-// FUNCTION: DELETE CONTACT
 function deleteContact(e) {
-    if (e.target.classList.contains("delete")) {
-        if (confirm("Are you sure?")) {
+    if (e.shiftKey) {
+        if (e.target.classList.contains("fa-user-minus")) {
+            e.target.parentElement.parentElement.remove();
+        } else if (e.target.classList.contains("delete")) {
             e.target.parentElement.remove();
         }
-    } else if (e.target.classList.contains("fas")) {
-        if (confirm("Are you sure?")) {
-            e.target.parentElement.parentElement.remove();
+    } else {
+        if (e.target.classList.contains("delete")) {
+            if (
+                confirm(
+                    "Are you sure?\n\nPS: You can hold shift and click delete to skip this confirmation.",
+                )
+            ) {
+                e.target.parentElement.remove();
+            }
+        } else if (e.target.classList.contains("fa-user-minus")) {
+            if (
+                confirm(
+                    "Are you sure?\n\nPS: You can hold shift and click delete to skip this confirmation.",
+                )
+            ) {
+                e.target.parentElement.parentElement.remove();
+            }
         }
     }
     if (contactList.children.length < 1) {
@@ -97,4 +113,41 @@ function deleteContact(e) {
     }
 }
 
-// NO CONTACTS MESSAGE IF THERE ARE NO CONTACTS
+function dragStart(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.add("dragging");
+    }
+}
+
+function dragEnd(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.remove("dragging");
+    }
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    const afterElement = getAfterElement(this, e.clientY);
+    const current = document.querySelector(".dragging");
+    if (afterElement == null) {
+        contactList.appendChild(current);
+    } else {
+        contactList.insertBefore(current, afterElement);
+    }
+}
+
+function getAfterElement(container, y) {
+    const listItems = [...container.querySelectorAll("li:not(.draggable)")];
+    return listItems.reduce(
+        (closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = y - box.top - box.height / 2;
+            if (offset < 0 && offset > closest.offset) {
+                return { offset: offset, element: child };
+            } else {
+                return closest;
+            }
+        },
+        { offset: Number.NEGATIVE_INFINITY },
+    ).element;
+}
